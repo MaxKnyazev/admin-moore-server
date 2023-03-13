6f858e620ec87c53d4a7af7af90f07284b89ad3d
MaxKnyazev <max.knyazev77@gmail.com>
2023-03-13 21:16:56 +0300

    2023-03-13 connect dayResults routes

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import { Guest } from './entities/guests/guests.model.js';
 import { guestsRoutes } from './entities/guests/guests.routes.js';
 import { User } from './entities/users/users.model.js';
 import { usersRoutes } from './entities/users/users.routes.js';
+import { DayResult } from './entities/dayResults/dayResults.model.js';
+import { dayResultsRoutes } from './entities/dayResults/dayResults.routes.js';
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -18,6 +20,7 @@ app.use(express.urlencoded({
 
 app.use('/guests', guestsRoutes);
 app.use('/users', usersRoutes);
+app.use('/dayResults', dayResultsRoutes);
 
 const start = async () => {
   try {
@@ -29,6 +32,9 @@ const start = async () => {
 
     await User.sync({});
     console.log(chalk.blue(`User model has been sync successfully...`));
+    
+    await DayResult.sync({});
+    console.log(chalk.blue(`DayResult model has been sync successfully...`));
 
     app.listen(PORT, () => {
       console.log(chalk.bgGreen(`Server started on port ${PORT}...`));
