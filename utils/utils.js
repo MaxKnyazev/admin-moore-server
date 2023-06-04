3c1c731c2c37f550e1838ee191ee2a19af481a97
MaxKnyazev <max.knyazev77@gmail.com>
2023-06-04 11:42:13 +0300

    2023-06-04 delete comments

--- utils/utils.js
+++ utils/utils.js
@@ -138,14 +138,6 @@ export const childTariff = minutes => {
   return result;
 }
 
-
-
-
-// =======================================================================================================
-
-
-
-
 export const convertMinutesToMoney = ({minutes, breakMinutes, isHoliday = false, tariffsId = '1'}) => {
   if (minutes === undefined || breakMinutes === undefined) {
     throw new Error('Значение времени не передано!');
@@ -186,56 +178,3 @@ export const convertMinutesToMoney = ({minutes, breakMinutes, isHoliday = false,
 
   return result;
 }
-
-
-//  ****************************************************************************************************
-//  ****************************************************************************************************
-//  ****************************************************************************************************
-
-
-// =======================================================================================================
-
-// export const standartTariff = minutes => {
-//   if (minutes === undefined) {
-//     throw new Error('Значение времени не передано!');
-//   }
-
-//   const result = {
-//     forPayment: 0,
-//     paymentDescription: ''
-//   }
-
-//   // Если гость пробыл час или меньше
-//   if (minutes <= HOUR) {
-//     result.forPayment = minutes * MONEY_PER_MINUTE_FIRST_HOUR;
-//     result.paymentDescription += `За 1-ый час :${minutes * MONEY_PER_MINUTE_FIRST_HOUR}: `;
-//     return result;
-//   }
-
-//   // Если гость пробыл два часа или меньше
-//   if (minutes <= HOUR * 2) {
-//     result.forPayment += HOUR * MONEY_PER_MINUTE_FIRST_HOUR;
-//     result.paymentDescription += `За 1-ый час :${HOUR * MONEY_PER_MINUTE_FIRST_HOUR}: `;
-//     minutes -= HOUR;
-
-//     result.forPayment += minutes * MONEY_PER_MINUTE_SECOND_HOUR;
-//     result.paymentDescription += `За 2-ой час :${minutes * MONEY_PER_MINUTE_SECOND_HOUR}: `;
-//     return result;
-//   }
-
-//   // Если гость пробыл больше двух часов
-//   result.forPayment += HOUR * MONEY_PER_MINUTE_FIRST_HOUR;
-//   result.paymentDescription += `За 1-ый час :${HOUR * MONEY_PER_MINUTE_FIRST_HOUR}: `;
-//   minutes -= HOUR;
-
-//   result.forPayment += HOUR * MONEY_PER_MINUTE_SECOND_HOUR;
-//   result.paymentDescription += `За 2-ой час :${HOUR * MONEY_PER_MINUTE_SECOND_HOUR}: `;
-//   minutes -= HOUR;
-
-//   result.forPayment += minutes * MONEY_PER_MINUTE_MORE_THAN_TWO_HOURS;
-//   result.paymentDescription += `За 3-ий час и более:${minutes * MONEY_PER_MINUTE_MORE_THAN_TWO_HOURS}: `;
-
-//   return result;
-// }
-
-// =======================================================================================================
